ae2e63771a7e4c7bba1c794083cd557d7b86ea93
perassilorenzo <perassi.lorenzo1804@gmail.com>
2026-08-18 21:33:21 +0200

    Cleanup: remove orphan assets and unreferenced files, fix invalid autoploop attribute, allow PORT env in server

--- server.js
+++ server.js
@@ -37,4 +37,5 @@ app.use(
   }),
 );
 
-app.listen(3000, () => console.log("http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
